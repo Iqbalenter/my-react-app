a9d592e3c1b3ff1cc05efa9351c61119ab5b06ea
Iqbal <108946263+Iqbalenter@users.noreply.github.com>
2024-09-08 19:49:38 +0700

    import

---
+++ src/components/Fragments/FormLogin.jsx
@@ -0,0 +1,41 @@
+import Button from "../Elements/Button";
+import InputForm from "../Elements/Input";
+import { useEffect, useRef } from "react";
+
+const FormLogin = () => {
+    const handleLogin = (event) => {
+      event.preventDefault();
+      localStorage.setItem('email', event.target.email.value);
+      localStorage.setItem('password', event.target.password.value);
+      console.log(event.target.email.value);
+      console.log(event.target.password.value);
+      console.log('login')
+      window.location.href = '/products'
+    };
+    const emailRef = useRef(null);
+
+    useEffect(() => {
+      emailRef.current.focus();
+    }, [])
+    return (
+        <form onSubmit={handleLogin}>
+        <InputForm 
+          label="email" 
+          type="email" 
+          placeholder="example@mail.com" 
+          name="email"
+          ref = {emailRef}
+        />
+        <InputForm 
+          label="Password" 
+          type="password" 
+          placeholder="*****" 
+          name="password"
+        />
+        
+        <Button classname="bg-blue-600 w-full" type="submit">Login</Button>
+      </form>
+    );
+}
+
+export default FormLogin
---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import LoginPage from './pages/login.jsx'
+import RegisterPage from './pages/register.jsx'
+import ErrorPage from './pages/404.jsx'
+import ProductsPage from './pages/products'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Hello World</h1>,
+    errorElement: <ErrorPage/>
+  },
+  {
+    path: '/login',
+    element: <LoginPage/>
+  },
+  {
+    path: '/register',
+    element: <RegisterPage/>
+  },
+  {
+    path: '/products',
+    element: <ProductsPage/>
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
