3bb5dd5ba9145f6b4f3d778c6a307c72ae2b0862
Iqbal <108946263+Iqbalenter@users.noreply.github.com>
2024-09-11 22:41:47 +0700

    getDetailProduct

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -5,4 +5,8 @@ export const getProducts = (callback) => {
     .then((res) => callback(res.data))
     .catch((err) => console.error("Error fetching products",err))
 }
-
+export const getDetailProduct = (id, callback) => {
+    axios.get(`https://fakestoreapi.com/products/${id}`)
+    .then((res) => callback(res.data))
+    .catch((err) => console.error("Error fetching products",err))
+}
